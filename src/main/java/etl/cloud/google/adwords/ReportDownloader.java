82e0127078107ff5457d3499d086a462c76911e3
ribitskiyb <this.bogdanr@gmail.com>
2017-07-11 19:26:01 +0300

    Initial commit

---
+++ src/main/java/etl/cloud/google/adwords/ReportDownloader.java
@@ -0,0 +1,110 @@
+package etl.cloud.google.adwords;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.analytics.Analytics;
+import com.google.api.services.analytics.Analytics.Data.Ga.Get;
+import com.google.api.services.analytics.AnalyticsScopes;
+import com.google.api.services.analytics.model.GaData;
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ReportDownloader {
+
+    public ReportDownloader(String p12KeyFilePath, String serviceAccountId, String applicationName)
+            throws GeneralSecurityException, IOException {
+        // Initialize an authorized analytics service object.
+
+        JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+        // Construct a GoogleCredential object with the service account email
+        // and p12 file downloaded from the developer console.
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        GoogleCredential credential = new GoogleCredential.Builder()
+                .setTransport(httpTransport)
+                .setJsonFactory(jsonFactory)
+                .setServiceAccountId(serviceAccountId)
+                .setServiceAccountPrivateKeyFromP12File(new File(p12KeyFilePath))
+                .setServiceAccountScopes(AnalyticsScopes.all())
+                .build();
+
+        // Construct the Analytics service object.
+        this.analytics = new Analytics.Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName(applicationName)
+                .build();
+    }
+
+    public void download(String ids, String startDate, String endDate, String metrics,
+                         String dimensions, String sort, String filters, String segment, String samplingLevel,
+                         String outputFile) throws IOException {
+
+        // Fail early if outputFile can't be written
+        try (FileWriter fw = new FileWriter(outputFile)) {
+            fw.write("");
+        }
+
+        Get request = analytics.data().ga()
+                .get(ids, startDate, endDate, metrics)
+                .setDimensions(dimensions)
+                .setSort(sort)
+                .setFilters(filters)
+                .setSegment(segment)
+                .setSamplingLevel(samplingLevel);
+
+        List<List<String>> data = fetchData(request);
+
+        writeCSV(data, outputFile, ',');
+    }
+
+    private static final int MAX_RESULTS_PER_REQUEST = (int) 1e4;  // API limit on amount of rows returned for a single request
+
+    private Analytics analytics;
+
+    // Fetches paginated data into a single collection
+    private List<List<String>> fetchData(Get request) throws IOException {
+        List<List<String>> fetched = new ArrayList<>();
+        // Reserve first row for headers
+        fetched.add(null);
+
+        request.setMaxResults(MAX_RESULTS_PER_REQUEST);
+        request.setStartIndex(1);
+
+        GaData page;
+        while (true) {
+            page = request.execute();
+            fetched.addAll(page.getRows());
+
+            int fetchedRows = fetched.size() - 1;
+            int totalRows = page.getTotalResults();
+            if (fetchedRows >= totalRows)
+                break;
+            request.setStartIndex(fetchedRows + 1);
+        }
+
+        // Put header row
+        List<String> headers = new ArrayList<>();
+        page.getColumnHeaders().forEach( h -> headers.add(h.getName()) );
+        fetched.add(0, headers);
+
+        return fetched;
+    }
+
+    private void writeCSV(List<List<String>> data, String path, char separator) throws IOException {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(path), separator);) {
+            for (List<String> row : data) {
+                writer.writeNext(row.toArray(new String[0]));
+            }
+        }
+    }
+
+}
